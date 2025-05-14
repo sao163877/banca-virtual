4f16c7354da28734b6d6466edcf450b6c54b6807
sao163877 <sao163877@gmail.com>
2025-05-14 18:46:10 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>🚀 Banca Virtual do Bem</h1>
+      <p>Seja bem-vindo! Em breve sua plataforma estará ativa e funcional.</p>
+    </div>
+  );
+}
